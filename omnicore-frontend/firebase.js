5017eedb93633f720a6e6f27f55dfb8fb9297203
Sheikh Shihab Hossain <meet2shihab18@gmail.com>
2025-06-04 12:19:15 +0600

    modified:   omnicore-frontend/firebase.js

--- omnicore-frontend/firebase.js
+++ omnicore-frontend/firebase.js
@@ -16,12 +16,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBOoJ-l1ptwx4A3M09yKyWBc4KZapqW67E",
-  authDomain: "omnicore-4b120.firebaseapp.com",
-  projectId: "omnicore-4b120",
-  storageBucket: "omnicore-4b120.firebasestorage.app",
-  messagingSenderId: "1071102207648",
-  appId: "1:1071102207648:web:37a8753350ee61f1554bc5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
